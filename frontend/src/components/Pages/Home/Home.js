bea9e9760d2d80a2dc432ee8a7786c6d217332d0
vanduc <duc52795@donga.edu.vn>
2022-11-21 22:20:40 +0700

    .

--- frontend/src/components/Pages/Home/Home.js
+++ frontend/src/components/Pages/Home/Home.js
@@ -1,10 +1,26 @@
+import './Home.css';
+import 'swiper/css/pagination';
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+
+// import required modules
+import Trainding from './Trainding/Trainding';
+import Trailer from './Trailer/Trailer';
 
 function Home() {
-  return (
-    <div className="Home">
-        <h1>HOME PAGES</h1>
-    </div>
-  );
+    return (
+        <div className="HomePage">
+            <div className='Trailer'>
+                <Trailer></Trailer>
+            </div>
+
+            <div>
+                <Trainding></Trainding>
+            </div>
+        </div>
+    );
 }
 
 export default Home;
